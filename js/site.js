e67a7f4e855199ef4a6e11f73fa5d9dda4bd2054
phitu2211 <phitu2211@gmail.com>
2020-09-14 10:58:37 +0700

    fix scroll back to top

--- js/site.js
+++ js/site.js
@@ -17,20 +17,20 @@ menu.addEventListener("click", function (e) {
 		subMenu1.classList.remove("show");
 		menu1.classList.remove("active");
 		setTimeout(function () {
-			subMenu.classList.contains("show") == true
-				? subMenu.classList.remove("show")
-				: subMenu.classList.add("show");
-			menu.classList.contains("active") == true
-				? menu.classList.remove("active")
-				: menu.classList.add("active");
+			subMenu.classList.contains("show") == true ?
+				subMenu.classList.remove("show") :
+				subMenu.classList.add("show");
+			menu.classList.contains("active") == true ?
+				menu.classList.remove("active") :
+				menu.classList.add("active");
 		}, 200);
 	} else {
-		subMenu.classList.contains("show") == true
-			? subMenu.classList.remove("show")
-			: subMenu.classList.add("show");
-		menu.classList.contains("active") == true
-			? menu.classList.remove("active")
-			: menu.classList.add("active");
+		subMenu.classList.contains("show") == true ?
+			subMenu.classList.remove("show") :
+			subMenu.classList.add("show");
+		menu.classList.contains("active") == true ?
+			menu.classList.remove("active") :
+			menu.classList.add("active");
 	}
 });
 
@@ -39,20 +39,20 @@ menu1.addEventListener("click", function (e) {
 		subMenu.classList.remove("show");
 		menu.classList.remove("active");
 		setTimeout(function () {
-			subMenu1.classList.contains("show") == true
-				? subMenu1.classList.remove("show")
-				: subMenu1.classList.add("show");
-			menu1.classList.contains("active") == true
-				? menu1.classList.remove("active")
-				: menu1.classList.add("active");
+			subMenu1.classList.contains("show") == true ?
+				subMenu1.classList.remove("show") :
+				subMenu1.classList.add("show");
+			menu1.classList.contains("active") == true ?
+				menu1.classList.remove("active") :
+				menu1.classList.add("active");
 		}, 200);
 	} else {
-		subMenu1.classList.contains("show") == true
-			? subMenu1.classList.remove("show")
-			: subMenu1.classList.add("show");
-		menu1.classList.contains("active") == true
-			? menu1.classList.remove("active")
-			: menu1.classList.add("active");
+		subMenu1.classList.contains("show") == true ?
+			subMenu1.classList.remove("show") :
+			subMenu1.classList.add("show");
+		menu1.classList.contains("active") == true ?
+			menu1.classList.remove("active") :
+			menu1.classList.add("active");
 	}
 });
 
@@ -61,20 +61,20 @@ menu2.addEventListener("click", function (e) {
 		subMenu3.classList.remove("show-1");
 		menu3.classList.remove("active");
 		setTimeout(function () {
-			subMenu2.classList.contains("show-1") == true
-				? subMenu2.classList.remove("show-1")
-				: subMenu2.classList.add("show-1");
-			menu2.classList.contains("active") == true
-				? menu2.classList.remove("active")
-				: menu2.classList.add("active");
+			subMenu2.classList.contains("show-1") == true ?
+				subMenu2.classList.remove("show-1") :
+				subMenu2.classList.add("show-1");
+			menu2.classList.contains("active") == true ?
+				menu2.classList.remove("active") :
+				menu2.classList.add("active");
 		}, 200);
 	} else {
-		subMenu2.classList.contains("show-1") == true
-			? subMenu2.classList.remove("show-1")
-			: subMenu2.classList.add("show-1");
-		menu2.classList.contains("active") == true
-			? menu2.classList.remove("active")
-			: menu2.classList.add("active");
+		subMenu2.classList.contains("show-1") == true ?
+			subMenu2.classList.remove("show-1") :
+			subMenu2.classList.add("show-1");
+		menu2.classList.contains("active") == true ?
+			menu2.classList.remove("active") :
+			menu2.classList.add("active");
 	}
 });
 
@@ -83,20 +83,20 @@ menu3.addEventListener("click", function (e) {
 		subMenu2.classList.remove("show-1");
 		menu2.classList.remove("active");
 		setTimeout(function () {
-			subMenu3.classList.contains("show-1") == true
-				? subMenu3.classList.remove("show-1")
-				: subMenu3.classList.add("show-1");
-			menu3.classList.contains("active") == true
-				? menu3.classList.remove("active")
-				: menu3.classList.add("active");
+			subMenu3.classList.contains("show-1") == true ?
+				subMenu3.classList.remove("show-1") :
+				subMenu3.classList.add("show-1");
+			menu3.classList.contains("active") == true ?
+				menu3.classList.remove("active") :
+				menu3.classList.add("active");
 		}, 200);
 	} else {
-		subMenu3.classList.contains("show-1") == true
-			? subMenu3.classList.remove("show-1")
-			: subMenu3.classList.add("show-1");
-		menu3.classList.contains("active") == true
-			? menu3.classList.remove("active")
-			: menu3.classList.add("active");
+		subMenu3.classList.contains("show-1") == true ?
+			subMenu3.classList.remove("show-1") :
+			subMenu3.classList.add("show-1");
+		menu3.classList.contains("active") == true ?
+			menu3.classList.remove("active") :
+			menu3.classList.add("active");
 	}
 });
 
@@ -106,3 +106,16 @@ function showAnswer(question) {
 	var answer = document.getElementById(answerId);
 	answer.classList.toggle("show-answer");
 }
+
+function scrollTopAnimated() {
+	$("html, body").animate({
+		scrollTop: "0"
+	});
+}
+$(window).scroll(function () {
+	if ($(this).scrollTop() > $(window).height() / 4) {
+		$('#toTop:hidden').fadeIn();
+	} else {
+		$('#toTop').fadeOut();
+	}
+});
